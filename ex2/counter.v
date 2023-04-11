7138129765c81a5723c9a7c1c5626d275364fc8b
Shahriar <77535132+Shahriar-0@users.noreply.github.com>
2023-04-11 16:48:54 +0430

    counter

---
+++ ex2/counter.v
@@ -0,0 +1,13 @@
+module counter(input clk, Clk_EN, inc_cnt, rst_cnt,
+               output reg [3:0] cnt_out, output Co);
+
+    always @(posedge clk) begin
+        if (rst_cnt)
+            cnt_out <= 4'd6;
+        else if (Clk_EN && inc_cnt)
+            cnt_out <= cnt_out + 1;
+    end
+
+    assign Co = &cnt_out;
+
+endmodule
