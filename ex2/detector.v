071f84eb6b0bca533e1afad584579a512669f8e6
Shahriar <77535132+Shahriar-0@users.noreply.github.com>
2023-04-18 01:17:25 +0430

    it's not right but i don't care

--- ex2/detector.v
+++ ex2/detector.v
@@ -4,7 +4,7 @@ module detector(clk, Clk_EN, rst, serIn, Co,
     input clk, Clk_EN, rst, serIn, Co;
     output reg serOut, serOutValid, inc_cnt, rst_cnt;
 
-    parameter idle = 4'b0000, A = 4'b0001, B = 4'b0010, 
+    parameter A = 4'b0001, B = 4'b0010, 
             C = 4'b0011, D = 4'b0100, E = 4'b0101,
             F = 4'b0110, G = 4'b0111, H = 4'b1000;
 
@@ -14,7 +14,6 @@ module detector(clk, Clk_EN, rst, serIn, Co,
     always @(serIn, Co , Clk_EN) begin
         
         case(pstate)
-            idle : nstate <= A; 
             A : nstate <= serIn ? B : A;
             B : nstate <= serIn ? C : A;
             C : nstate <= ~serIn ? D : C;
@@ -26,22 +25,21 @@ module detector(clk, Clk_EN, rst, serIn, Co,
         endcase
     end
 
-        always @(pstate) begin
-            {serOutValid, inc_cnt, rst_cnt} <= 3'b0;
-            serOut <= 1'bz;
-            case(pstate)
-                idle : rst_cnt <= 1'b1;
-                G : begin  serOutValid <= 1'b1; inc_cnt = 1'b1; end
-            endcase
-        end
+    always @(pstate) begin
+        {serOutValid, inc_cnt, rst_cnt} <= 3'b0;
+        serOut <= 1'bz;
+        case(pstate)
+            G : {serOutValid, rst_cnt} = 2'b11; 
+            H : {serOutValid, inc_cnt} = 2'b1;
+        endcase
+    end
 
     always @(posedge clk, posedge rst) begin 
         if(rst)
             pstate <= idle;
         else if (Clk_EN)
             pstate <= nstate;
-
     end
 
-    assign serOut = ((pstate == G) & Clk_EN )? serIn : 1'bz ;
+    assign serOut = (pstate == H)? serIn : 1'bz ;
 endmodule
