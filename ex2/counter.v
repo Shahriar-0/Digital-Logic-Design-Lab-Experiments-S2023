97e122c1d3d8a2eb75bf9ff78e4fb8779fdac5dd
Shahriar <77535132+Shahriar-0@users.noreply.github.com>
2023-04-18 21:17:05 +0430

    deleted old files

--- ex2/counter.v
+++ ex2/counter.v
@@ -1,20 +1,18 @@
-module counter(clk, Clk_EN, inc_cnt,
-               rst_cnt, cnt_out, Co);
+module counter (clk, clk_en, rst_cnt,
+                inc_cnt, co, cnt_out);
+                
+    input clk, clk_en, rst_cnt, inc_cnt;
     
-    input clk, Clk_EN, inc_cnt, rst_cnt;
+    output co;
     output reg [3:0] cnt_out;
-    output Co;
-
 
     always @(posedge clk) begin
-        if (rst_cnt) begin
+        if (rst_cnt)
             cnt_out <= 4'd6;
-            Co <= 1'b0;
-        end
-        else if (Clk_EN && inc_cnt)
-           {Co, cnt_out} <= cnt_out + 1;
-    end
-
-    // assign Co = &cnt_out;
+        else if(clk_en && inc_cnt)
+            cnt_out = cnt_out + 1;
+    end 
+    
+    assign co = &cnt_out;
 
 endmodule
