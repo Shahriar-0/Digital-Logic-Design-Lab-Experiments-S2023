daf7ce7ee4f6e7e5965b5858abe788b6b408f907
Shahriar <77535132+Shahriar-0@users.noreply.github.com>
2023-04-22 01:59:25 +0430

    hey fuck you

--- ex2/code/OTHFSM_tb.v
+++ ex2/code/OTHFSM_tb.v
@@ -1,30 +1,29 @@
-module tb();
-    reg clk, clkPB, serIn, rst;
+module OTHFSM_tb();
+    reg clk, Clk_EN, serIn, rst;
     wire serOut, serOutValid;
     wire [3:0] cnt_out;
-    
+
     OTHFSM OTHFSM_instance(
-        .clk(clk), .rst(rst), .clkPB(clkPB), 
+        .clk(clk), .rst(rst), .Clk_EN(Clk_EN), 
         .serIn(serIn), .serOut(serOut), 
         .serOutValid(serOutValid), .cnt_out(cnt_out)
-    );                        
+    );
 
     always #5 clk = ~clk;
-    always #50 clkPB = ~clkPB;
     
     initial begin
-        clk = 0;
-        rst = 1;
-        clkPB = 0;
-        #50 rst = 0; 
-        serIn = 1;
-        #100 serIn = 1;
-        #100 serIn = 0;
-        #100 serIn = 1;
-        #100 serIn = 0;
-        #100 serIn = 1;
-        #500 serIn = 0;
-        #200 serIn = 1;
+        clk = 1'b0;
+        rst = 1'b1;
+        Clk_EN = 1'b1;
+        #1 rst = 1'b0; 
+        serIn = 1'b1;
+        #10 serIn = 1'b1;
+        #10 serIn = 1'b0;
+        #10 serIn = 1'b1;
+        #10 serIn = 1'b0;
+        #10 serIn = 1'b1;
+        #10 serIn = 1'b0;
+        #10 serIn = 1'b1;
         #2000 $stop;
     end
 endmodule
