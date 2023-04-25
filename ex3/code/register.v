fb1723a28fea2bca060829c1f65e53b41b93e579
Shahriar <77535132+Shahriar-0@users.noreply.github.com>
2023-04-25 16:30:22 +0430

    register

---
+++ ex3/code/register.v
@@ -0,0 +1,14 @@
+module Register #(parameter N = 8, 
+                  parameter [N-1:0] Init = {N{1'b0}})
+                 (loadData, load, clr, clk, rst, out);
+
+    input [N-1:0] loadData,
+    input load, clr, clk, rst,
+    output reg [N-1:0] out;
+
+    always @(posedge clk or posedge rst) begin
+        if (rst or clr) out <= Init;
+        else if (load) out <= loadData;
+    end
+
+endmodule
