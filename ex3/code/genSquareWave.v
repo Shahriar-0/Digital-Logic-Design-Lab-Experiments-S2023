db628ec3578f28ddfa56f85524cd6cafbe7ab3fa
Shahriar <77535132+Shahriar-0@users.noreply.github.com>
2023-04-25 17:59:39 +0430

    NO MERGE

--- ex3/code/genSquareWave.v
+++ ex3/code/genSquareWave.v
@@ -3,9 +3,11 @@ module GenSquareWave(cnt, out);
     input [7:0] cnt,
     output reg [7:0] out
 
+    parameter [7:0] HALF = 8'd128, FULL = 8'd255, ZERO = 8'd0;
+
     always @(cnt) begin
-        if (cnt < 8'd128) out = 8'd255;
-        else out = 8'd0;
+        if (cnt < HALF) out = FULL;
+        else out = ZERO;
     end
     
 endmodule
