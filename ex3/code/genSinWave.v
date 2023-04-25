ade227ef98a41f2d33b83be089ce3a66e725d4b3
Shahriar <77535132+Shahriar-0@users.noreply.github.com>
2023-04-25 18:13:14 +0430

    delete clr in register

--- ex3/code/genSinWave.v
+++ ex3/code/genSinWave.v
@@ -1,17 +1,18 @@
-module GenSineWave(
+module GenSineWave(cnt, clk, rst, out);
+
     input [7:0] cnt,
     input clk, rst,
     output [7:0] out
-);
+
     wire signed [15:0] sinLdData, cosLdData;
     wire signed [15:0] sinOut, cosOut, pSinOut, pCosOut;
     wire regLd;
     assign regLd = 1'b1;
 
-    Register #(16) sin(.loadData(sinLdData), .load(regLd), .clr(1'b0), .clk(clk), .rst(rst), .out(sinOut));
-    Register #(16) cos(.loadData(cosLdData), .load(regLd), .clr(1'b0), .clk(clk), .rst(rst), .out(cosOut));
-    Register #(16) prevSin(.loadData(sinOut), .load(regLd), .clr(1'b0), .clk(clk), .rst(rst), .out(pSinOut));
-    Register #(16) prevCos(.loadData(cosOut), .load(regLd), .clr(1'b0), .clk(clk), .rst(rst), .out(pCosOut));
+    Register #(16) sin(.loadData(sinLdData), .load(regLd), .clk(clk), .rst(rst), .out(sinOut));
+    Register #(16) cos(.loadData(cosLdData), .load(regLd), .clk(clk), .rst(rst), .out(cosOut));
+    Register #(16) prevSin(.loadData(sinOut), .load(regLd), .clk(clk), .rst(rst), .out(pSinOut));
+    Register #(16) prevCos(.loadData(cosOut), .load(regLd), .clk(clk), .rst(rst), .out(pCosOut));
 
     wire signed [15:0] apCos, aSin;
     assign apCos = pCosOut >>> 6;
