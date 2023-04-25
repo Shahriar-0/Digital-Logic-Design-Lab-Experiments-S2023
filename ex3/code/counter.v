2c05721802bc9b795f18e4db95a648cd90af26e8
Shahriar <77535132+Shahriar-0@users.noreply.github.com>
2023-04-25 18:30:59 +0430

    fixed full

--- ex3/code/counter.v
+++ ex3/code/counter.v
@@ -4,10 +4,12 @@ module Counter #(parameter N = 8)
     input clk, rst;
     output reg [N - 1:0] out;
     output co;
+    
     always @(posedge clk or posedge rst) begin
         if (rst) out <= {N{1'b0}};
+        else out = out + 1'b1;
     end
-    assign out = out + 1'b1;
+
     assign co = &{out};
 
 endmodule
--- ex3/code/genFullWaveRectified.v
+++ ex3/code/genFullWaveRectified.v
@@ -1,9 +1,12 @@
-module GenSinFullWave(
+module GenSinFullWave #(parameter [7:0] HALF = 8'128)
+                       (sine, out);
+
     input [7:0] sine,
     output reg [7:0] out
-);
+
     always @(sine) begin
-        if (sine < 8'd128) out = 8'd255 - sine;
-        else out = sine;
+        if (sine < 8'd128) out = sine;
+        else out = -sine;
     end
+
 endmodule
