46dddf42ae22a0bcdaf1d1c1e6ebc0c3ede4681f
elahekhodaverdi <102658681+elahekhodaverdi@users.noreply.github.com>
2023-04-25 18:05:52 +0430

    clean counter

--- ex3/code/counter.v
+++ ex3/code/counter.v
@@ -1,18 +1,13 @@
 module Counter #(parameter N = 8)
-                (loadData, load, en, clr, clk, rst, out, co);
+                (clk, rst, out, co);
 
-    input [N - 1:0] loadData;
-    input load, en, clr, clk, rst;
+    input clk, rst;
     output reg [N - 1:0] out;
     output co;
-
     always @(posedge clk or posedge rst) begin
         if (rst) out <= {N{1'b0}};
-        else if (clr) out <= {N{1'b0}};
-        else if (load) out <= loadData;
-        else if (en) out <= out + 1'b1;
     end
-
-    assign co = &{out, en};
+    assign out = out + 1'b1;
+    assign co = &{out};
 
 endmodule
