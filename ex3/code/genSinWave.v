11dc6c384c407e2ce7dc6bf6a18ff5076958a359
Shahriar-0 <attarshahriar@gmail.com>
2023-04-28 21:02:10 +0430

    changed sine calculation

--- ex3/code/genSinWave.v
+++ ex3/code/genSinWave.v
@@ -4,21 +4,21 @@ module GenSineWave (cnt, clk, rst, out);
     input clk, rst;
     output [7:0] out;
 
-    wire signed [15:0] sin_load_data, cos_load_data;
-    wire signed [15:0] sin_reg_output, cos_reg_out, previous_sin_reg_output, pcos_reg_out;
+    reg signed [15:0] sine_load_data, cos_load_data;
+    wire signed [15:0] sine_output, cos_out;
 
-    Register #(16) previous_sin(.load_data(sin_reg_output), .clk(clk), .rst(rst), .out(previous_sin_reg_output));
-    Register #(.N(16), .INIT(16'd30000)) previous_cos(.load_data(cos_reg_out), .clk(clk), .rst(rst), .out(pcos_reg_out));
+    wire [5:0] cos_shift, sine_shift;
+    assign cos_shift = 6{cos_out[15]};
+    assign sine_shift = 6{sine_out[15]};
 
-    Register #(16) sin(.load_data(sin_load_data), .clk(clk), .rst(rst), .out(sin_reg_output));
-    Register #(.N(16), .INIT(16'29993)) cos(.load_data(cos_load), .clk(clk), .rst(rst), .out(cos_reg_out)); // the 29993 is the value of cos(1)
+    Register #(16) sin(.load_data(sin_load_data), .clk(clk), .rst(rst), .out(sine_output));
+    Register #(.N(16), .INIT(16'd30000)) cos(.load_data(cos_load), .clk(clk), .rst(rst), .out(cos_out)); // the 30000 is the value of cos(0)
 
-    wire signed [15:0] apCos, aSin;
-    assign apCos = {6{pcos_reg_out[15]}, pcos_reg_out[9:0]};
-    assign aSin = {6{sin_load_data[15]}, sin_load_data[9:0]};
+    always @(posedge clk) begin
+        sine_load_data = sine_output + {cos_shift, cos_out[9:0]};
+        cos_load_data = cos_output - {sine_shift, sine_load_data[9:0]}; 
+    end
 
-    Adder      #(16) adr(.a(previous_sin_reg_output), .b(apCos), .out(sin_load_data));
-    Subtractor #(16) sub(.a(pcos_reg_out), .b(aSin), .out(cos_load));
-
-    assign out = sin_reg_output[15:8];
+    assign out = sine_output[15:8];
+    
 endmodule
