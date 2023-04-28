e31725bab9781112c0d5730e4e562d3b4ba38eba
Shahriar-0 <attarshahriar@gmail.com>
2023-04-28 20:49:56 +0430

    changed reciprocal formula

--- ex3/code/genReciprocalWave.v
+++ ex3/code/genReciprocalWave.v
@@ -1,16 +1,12 @@
 module GenReciprocalWave #(parameter [7:0] INITIAL = 8'd8,
                                            FULL = 8'd255,
-                                           X_COEFFICIENT_DENOMINATOR = 8'd3, 
-                                           THRESHOLD = 8'd254)
+                                           X_COEFFICIENT_DENOMINATOR = 8'd3)
                           (cnt, out);
 
     input [7:0] cnt;
     output reg [7:0] out;
 
-    // 255 / (32 - x / 8) 
-    always @(cnt) begin
-        if (cnt >= THRESHOLD) out = INITIAL;
-        else out = FULL / (cnt >> X_COEFFICIENT_DENOMINATOR);
-    end
+    // 255 / (256 - x) 
+    assign out = FULL / (FULL + 8'd1 - (cnt >> X_COEFFICIENT_DENOMINATOR));
 
 endmodule
