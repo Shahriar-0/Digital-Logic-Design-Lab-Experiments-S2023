ca823b7fab415a1c084b75917cbfc601882e3b65
Shahriar <77535132+Shahriar-0@users.noreply.github.com>
2023-04-29 12:44:52 +0430

    i'm tired tonight

--- ex3/code/WaveformGenerator.v
+++ ex3/code/WaveformGenerator.v
@@ -29,6 +29,7 @@ module WaveformGenerator(clk, rst, slc, out);
             3'b100: out = full_wave_rectified_output;    // this is not checked
             3'b101: out = half_wave_rectified_output;    // this also
             3'b110: out = DDS_output;
+            3'b111: out = 
             default: out = 8'bz;
         endcase
     end
---
+++ ex3/code/frequencySelector.v
@@ -0,0 +1,9 @@
+module FreqSelector #(parameter N = 9)
+                     (sel, clk, rst, co);
+    
+    input [4:0] sel;
+    input clr, clk, rst;
+    output co;
+
+    
+endmodule
