903a489ccda212d1869da7e7d0e2d50eff7429d7
Shahriar-0 <attarshahriar@gmail.com>
2023-04-30 01:05:11 +0430

    ok i can't fix duty cycle formula is not right

--- ex3/code/genFullWaveRectified.v
+++ ex3/code/genFullWaveRectified.v
@@ -1,11 +1,12 @@
 module GenFullWaveRectified #(parameter [7:0] HALF = 8'd128)
                        (sine, out, cnt);
 
-    input [7:0] sine, cnt;
+    input signed [7:0] sine;
+    input [7:0] cnt;
     output reg [7:0] out;
 
     always @(sine or cnt) begin
-        if (cnt < HALF) out = sine;
+        if (sine > 0) out = sine;
         else out = -sine;
     end
 
--- ex3/code/genHalfWaveRectified.v
+++ ex3/code/genHalfWaveRectified.v
@@ -2,11 +2,12 @@ module GenHalfWaveRectified #(parameter [7:0] HALF = 8'd127,
                                         ZERO = 8'd0)
                        (sine, out, cnt);
 
-    input [7:0] sine, cnt;
+    input signed [7:0] sine;
+    input [7:0] cnt;
     output reg [7:0] out;
 
     always @(sine or cnt) begin
-        if (cnt < HALF) out = sine;
+        if (sine > 0) out = sine;
         else out = ZERO;
     end
 
