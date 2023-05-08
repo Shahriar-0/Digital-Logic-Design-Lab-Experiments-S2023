7dff770c0a780612f02d5e1a43fe962ddb410245
elahekhodaverdi <102658681+elahekhodaverdi@users.noreply.github.com>
2023-05-08 22:55:06 +0430

    a

--- ex3/code/WG.v
+++ ex3/code/WG.v
@@ -1,22 +1,21 @@
 
-module WG(clk, rst, square, triangle, full_wave_rectified, half_wave_rectified, reciprocal,sine);
+module WG(clk, rst, square_out, triangle_out, full_wave_rectified_out, half_wave_rectified_out, reciprocal_out,sine_out);
 
-    input clk, rst;
-    output reg [7:0] out;
+    input clk, rst; 
     wire [7:0] cnt;
     wire co;
 
-    output [7:0] square, triangle, full_wave_rectified, half_wave_rectified, reciprocal,sine;
+    output [7:0] square_out, triangle_out, full_wave_rectified_out, half_wave_rectified_out, reciprocal_out,sine_out;
     wire signed [7:0] sine_f;
-    assign sine = sine_f + 8'd127;
+    assign sine_out = sine_f + 8'd127;
     Counter counter(.clk(clk), .rst(rst), .out(cnt), .co(co));
 
-    GenReciprocalWave    reciprocal(.cnt(cnt), .out(reciprocal));
-    GenSquareWave        square(.cnt(cnt), .out(square));
-    GenTriangleWave      triangle(.cnt(cnt), .out(triangle));
+    GenReciprocalWave    reciprocal(.cnt(cnt), .out(reciprocal_out));
+    GenSquareWave        square(.cnt(cnt), .out(square_out));
+    GenTriangleWave      triangle(.cnt(cnt), .out(triangle_out));
     GenSineWave          sine(.cnt(cnt), .out(sine_f), .clk(clk), .rst(rst));
-    GenFullWaveRectified full_rectified(.cnt(cnt), .out(full_wave_rectified), .sine(sine_f));
-    GenHalfWaveRectified half_rectified(.cnt(cnt), .out(half_wave_rectified),  .sine(sine_f));
+    GenFullWaveRectified full_rectified(.cnt(cnt), .out(full_wave_rectified_out), .sine(sine_f));
+    GenHalfWaveRectified half_rectified(.cnt(cnt), .out(half_wave_rectified_out),  .sine(sine_f));
 
     // always @(slc or reciprocal or square or
     //          triangle or sine or 
