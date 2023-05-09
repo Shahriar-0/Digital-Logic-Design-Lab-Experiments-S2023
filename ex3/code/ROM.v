342678f7514af3aeaca56ea1007ac55f076150b7
elahekhodaverdi <102658681+elahekhodaverdi@users.noreply.github.com>
2023-05-09 16:05:36 +0430

    rom modules

---
+++ ex3/code/ROM.v
@@ -0,0 +1,19 @@
+module rom_module(phase_cntrl, data_out);
+
+    input [1:0] phase_cntrl;
+    output reg [7:0] data_out;
+    reg [7:0] address;
+
+    reg [7:0] memory [255:0];
+
+    initial begin
+        $readmemb("sine.mif", memory);
+        address = 8'b0;
+    end
+
+    always @(posedge clk) begin
+        address = address + phase_cntrl;
+        data_out = memory[phase_cntrl];
+    end
+
+endmodule
