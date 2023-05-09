18991eaacfcf25f4d117b06cacf205c688e2a95e
Shahriar <77535132+Shahriar-0@users.noreply.github.com>
2023-05-09 14:02:28 +0430

    fuck register and dac

--- ex3/code/genSinWave.v
+++ ex3/code/genSinWave.v
@@ -7,10 +7,12 @@ module GenSineWave (cnt, clk, rst, out);
     wire signed [15:0] sine_load_data, cos_load_data;
     wire signed [15:0] sine_output, cos_output, prev_sine_output, prev_cos_output;
 
-    Register sin(.load_data(sine_load_data), .clk(clk), .rst(rst), .out(sine_output));
-    Register #(.N(16), .INIT(16'd29993)) cos(.load_data(cos_load_data), .clk(clk), .rst(rst), .out(cos_output)); // the 29993 is the value of cos(1)
-    Register prev_sin(.load_data(sine_output), .clk(clk), .rst(rst), .out(prev_sine_output));
-    Register #(.N(16), .INIT(16'd30000)) prev_cos(.load_data(cos_output), .clk(clk), .rst(rst), .out(prev_cos_output)); // the 30000 is the value of cos(0)
+    Register sin(.load_data(sine_load_data), .clk(clk), .rst(rst), .out(sine_output), .load(1'b1));
+    Register #(.N(16), .INIT(16'd29993)) cos(.load_data(cos_load_data), .clk(clk), .rst(rst), .out(cos_output), .load(1'b1)); 
+    // the 29993 is the value of cos(1)
+    Register prev_sin(.load_data(sine_output), .clk(clk), .rst(rst), .out(prev_sine_output), .load(1'b1));
+    Register #(.N(16), .INIT(16'd30000)) prev_cos(.load_data(cos_output), .clk(clk), .rst(rst), .out(prev_cos_output), .load(1'b1)); 
+    // the 30000 is the value of cos(0)
 
     wire signed [15:0] shifted_cos, shifted_sine;
     wire [5:0] cos_shift, sine_shift;
