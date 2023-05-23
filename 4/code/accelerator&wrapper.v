cb7761e12968fc741655f5d9f55399bbbd1285ce
elahekhodaverdi <102658681+elahekhodaverdi@users.noreply.github.com>
2023-05-23 18:57:45 +0330

    edit topmodule

--- 4/code/accelerator&wrapper.v
+++ 4/code/accelerator&wrapper.v
@@ -1,4 +1,10 @@
-module accelerator_wrappers(input clk , rst, start, input [1:0] U, input [5:0] V,output done, output out);
+module accelerator_wrappers(input clk , rst, start, input [1:0] U, input [5:0] V,output done, output [20:0] out);
 
+    datapath(clk, rst, ld, sh_en, eng_start, ui_reg_ld ,
+                    V, U,
+                    eng_done, wr_data);
 
+    controller(clk, rst, eng_done, start,
+                  ld, sh_en, wr_req, 
+                  eng_start, ui_reg_ld, .done(done));
 endmodule
