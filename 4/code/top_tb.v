f8fcf7f2756ecbbf451f0646aa6fa809f1ead421
Shahriar <77535132+Shahriar-0@users.noreply.github.com>
2023-05-23 18:10:58 +0330

    top tb

--- 4/code/top_tb.v
+++ 4/code/top_tb.v
@@ -5,8 +5,18 @@ module top_tb();
     wire done, wr_req;
     wire [20:0] wr_data;
     
+    accelerator_wrappers accwr(.clk(clk), .rst(rst), .start(start), 
+                               .U(U), .V(V), .done(done), 
+                               .wr_req(wr_req), .wr_data(wr_data))
+
     always #5 clk = ~clk;
 
-    
+    initial begin
+        rst = 1'b1; clk = 1'b0; start = 1'b0;
+        #30 rst = 1'b0;
+        #10 start = 1'b1;
+        #20 start = 1'b0;
+        #1000 $stop;
+    end
 
 endmodule
