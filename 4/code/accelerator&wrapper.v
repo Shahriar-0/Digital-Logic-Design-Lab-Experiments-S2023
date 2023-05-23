4a44294526b8fee96956c4ca156ed28649aff527
elahekhodaverdi <102658681+elahekhodaverdi@users.noreply.github.com>
2023-05-23 19:05:20 +0330

    fix again

--- 4/code/accelerator&wrapper.v
+++ 4/code/accelerator&wrapper.v
@@ -1,6 +1,6 @@
 module accelerator_wrappers(input clk , rst, start, input [1:0] U, input [5:0] V,output done, wr_req, output [20:0] wr_data);
 
-    wire ld, sh_en, eng_start, ui_reg_ld, eng_done,
+    wire ld, sh_en, eng_start, ui_reg_ld, eng_done;
     datapath(.clk(clk), .rst(rst), .ld(ld), .sh_en(sh_en), .eng_start(eng_start), .ui_reg_ld(ui_reg_ld) ,
                     .V(V), .U(U),
                     .eng_done(eng_done), .wr_data(wr_data));
